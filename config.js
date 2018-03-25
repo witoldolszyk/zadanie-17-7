e73b508d593ceb3d117bb22cb6e1f67cca26e819
Witold Olszyk <witoldolszyk@gmail.com>
2018-03-25 04:27:20 +0200

    add all needed things

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports =  {
+    'GOOGLE_CLIENT_ID': '259811539630-df2ilkmb6c32q1mqkdndn1bcppr62iml.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '9kK2kWrPaY7Uth7XUkIw1hgK',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
+var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret:config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('main'));
+
+
+//app routes
+app.get('/', function(req, res){
+    res.render('login-form', { user: req.user });
+});
+
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
+});
+//Passport routes
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope : ['profile', 'email']
+    }));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
+
+app.listen(3000);
+app.use(function(req, res, next){
+  res.status(404).send('Wybacz nie mogliśmy odnaleść tego, czego żądzasz');
+});
